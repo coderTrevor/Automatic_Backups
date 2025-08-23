e9922939466e940b597e42f49c3685a310e20197
Trevor Thompson <codertrevor@gmail.com>
2025-08-23 15:17:45 -0400

    Refactor Core.cs:
    - Extract code for DeleteOldestToggle to DeleteOldestToggle.cs.

--- Core.cs
+++ Core.cs
@@ -236,25 +236,4 @@ namespace AutomaticBackups
             });
         }
     }
-
-    public class DeleteOldestToggle : SettingsToggle
-    {
-        protected GameObject retainedCountSlider = null;
-
-        public void Start()
-        {
-            Core.Log("start called");
-            // Find the retained count slider and show/hide it based on our enableAutoDelete setting
-            retainedCountSlider = GameObject.Find("RetainedCount");
-            retainedCountSlider.SetActive(Core.enableAutoDelete.Value);
-            Core.Log("start done");
-        }
-
-        protected override void OnValueChanged(bool value)
-        {
-            retainedCountSlider.SetActive(value);
-            Core.Log($"{value}");
-            Core.enableAutoDelete.Value = value;
-        }
-    }
 }
---
+++ DeleteOldestToggle.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using ScheduleOne.UI.Settings;
+
+#if IL2CPP
+using Il2CppScheduleOne.UI.MainMenu;
+#elif MONO
+using ScheduleOne.UI.MainMenu;
+#else
+    // Other configs could go here
+#endif
+
+namespace AutomaticBackups
+{
+    public class DeleteOldestToggle : SettingsToggle
+    {
+        protected GameObject retainedCountSlider = null;
+
+        public void Start()
+        {
+            Core.Log("start called");
+            // Find the retained count slider and show/hide it based on our enableAutoDelete setting
+            retainedCountSlider = GameObject.Find("RetainedCount");
+            retainedCountSlider.SetActive(Core.enableAutoDelete.Value);
+            Core.Log("start done");
+        }
+
+        protected override void OnValueChanged(bool value)
+        {
+            retainedCountSlider.SetActive(value);
+            Core.Log($"{value}");
+            Core.enableAutoDelete.Value = value;
+        }
+    }
+}
